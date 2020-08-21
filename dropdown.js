b07c83998fbd582fb02bb0d02374e7eddbbfc480
Saad Rassam <smrassam@gmail.com>
2020-08-20 22:36:02 -0400

    linking drop down

--- dropdown.js
+++ dropdown.js
@@ -2,3 +2,14 @@ var mainEl = "Main Page";
 var portEl = "Portfolio Page";
 var contEl = "Contact";
 
+function myFunction() {
+    document.getElementById("Main").classList.toggle("show");
+}
+
+function myFunction() {
+    document.getElementById("Contact").classList.toggle("show");
+}
+
+function myFunction() {
+    document.getElementById("Portfolio").classList.toggle("show");
+}
